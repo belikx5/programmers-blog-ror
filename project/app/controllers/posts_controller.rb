1d11080ef0410a7d8b701f70b77728ebf4a74b7d
belikx5 <i.bilicenko@gmail.com>
2020-04-24 23:41:04 +0300

    Add bug fixes

--- project/app/controllers/posts_controller.rb
+++ project/app/controllers/posts_controller.rb
@@ -1,23 +1,20 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  # GET /posts
-  # GET /posts.json
+
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
+
   def edit
   end
 
@@ -28,8 +25,8 @@ class PostsController < ApplicationController
       @posts = Post.all
     end
   end
-    # POST /posts
-  # POST /posts.json
+
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -44,8 +41,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -58,8 +53,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.comments.destroy
     @post.destroy
@@ -70,12 +63,10 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :body, :photo, :user_id, :tag_list)
     end
