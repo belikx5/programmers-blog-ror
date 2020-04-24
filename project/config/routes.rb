151be309fee1c7b39e3ac7d64bf793fdf75e3dce
belikx5 <i.bilicenko@gmail.com>
2020-04-24 23:44:35 +0300

    Add NORMAL routes

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -7,10 +7,5 @@ Rails.application.routes.draw do
   get '/tagged', to: "posts#tagged", as: :tagged
   get 'about' => 'pages#about' 
   get 'account' => 'pages#account'
-  get 'it_posts' => 'posts#it_posts'
-  get 'programming_posts' => 'posts#programming_posts'
-  get 'studying_posts' => 'posts#studying_posts'
-  get 'technologies_posts' => 'posts#technologies_posts'
-  get 'gaming_posts' => 'posts#gaming_posts'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
